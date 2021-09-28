93fc25f8851e24d11cdfab6aef588c33aacf8ca4
Saksham Negi <negisaksham363@gmail.com>
2021-09-28 19:46:57 +0530

    More solutions added

---
+++ sorting and searching/movie-festival.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL) 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+bool comp( pair<int,int> p1, pair<int,int> p2){
+    return (p1.second < p2.second);
+}
+void solve(int k){
+    int n; cin>>n;
+    vector<pair<int,int>> v;
+    for(ll i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        v.push_back(make_pair(x,y));
+    }
+    sort(v.begin(),v.end(),comp);
+    int ans=0;
+    int curr = 0;
+    for(int i=0;i<n;i++){
+        if(v[i].first >= curr){
+            ans++;
+            curr = v[i].second;
+        }
+    }
+    cout<<ans<<endl;
+}
+int main() {
+    fast;
+    int t=1;
+    // cin>>t;
+    int k=1;
+    while(t--){
+        solve(k);
+    }
+}
